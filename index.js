20b6c5130968927df1ac10881373779371cec80f
AustinAdams11 <austinadams011@gmail.com>
2025-07-21 13:00:06 -0400

    functional build 1.1

--- index.js
+++ index.js
@@ -26,17 +26,20 @@ async function fetchItem(itemName = null) {
 
   try {
     const res = await fetch(`https://pokeapi.co/api/v2/item/${input}`);
-    if (!res.ok) throw new Error("Item not found");
+    if (!res.ok) throw new Error("Item not found"); //If no response, go to the catch block
 
     const data = await res.json();
     const name = data.name;
-    const img = data.sprites?.default || '';
+    const img = data.sprites?.default || ''; //First use of optional chaining so that it doesn't break when it can't find anything
     const effect = data.effect_entries.find(e => e.language.name === 'en')?.short_effect || 'No effect info.';
+    const cost = data.cost;
 
+    //displays item name, image, cost, and effect 
     display.innerHTML = `
       <h2>${name}</h2>
-      ${img ? `<img src="${img}" alt="${name}" class="item-img" />` : ''}
+      ${img ? `<img src="${img}" alt="${name}" class="item-img" />` : ''} 
       <p class="effect">${effect}</p>
+      <p class="cost">cost= $${cost}</>
     `;
   } catch (error) {
     display.innerHTML = `<p style="color: red;">${error.message}</p>`;
@@ -44,5 +47,29 @@ async function fetchItem(itemName = null) {
   }
 }
 
-// Load the list when the page loads
+// Load the item list when the page loads
 window.onload = fetchItemList;
+
+
+
+// //css test stuff
+//  #map {
+//       padding-left: 20px;
+//       margin-top: 1200px;
+//       margin-right: 100px
+//       width: 100px
+//     }
+//     #celadon {
+//       margin-right: 200px;
+//       margin-top: 1200px;
+//       width: 100px
+//     }
+
+// </div>
+//    <div id="map">
+//     <img src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-blue-version/5/52/Pokem_170.jpg?width=325&format=jpg&auto=webp&quality=80">
+//   </div>
+
+//   <div id="celadon">
+//     <img src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-blue-version/0/01/Pokem_173.jpg?width=325&format=jpg&auto=webp&quality=80">
+//   </div>
