1bee69a02957189bcc4f919694c4dfe520fea590
AustinAdams11 <austinadams011@gmail.com>
2025-07-21 16:40:43 -0400

    functional build 1.2

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # api-project
+
+This is a web application that fetches and displays a list of Pokémon items using the [PokéAPI](https://pokeapi.co/). Users can click on an item from the list to see its image, effect, and cost. Users can also search for an item if they know the number or name. Just remember to put a - in between two word names to return the items information.
+
+---
+
+# How to Download the Project
+
+   git clone https://github.com/AustinAdams11/api-project.git
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ async function fetchItemList() {
   try {
     const res = await fetch('https://pokeapi.co/api/v2/item?limit=50');
     const data = await res.json();
-
+// adds items to the sidebar and allows the fetchItem() to run when clicked
     listDiv.innerHTML = data.results.map(item => `
       <div class="item-name" onclick="fetchItem('${item.name}')">
         ${item.name}
@@ -17,12 +17,12 @@ async function fetchItemList() {
 }
 
 // Fetch and display individual item details
-async function fetchItem(itemName = null) {
+async function fetchItem(itemName) {
   const input = itemName || document.getElementById('itemInput').value.toLowerCase().trim();
   const display = document.getElementById('itemDisplay');
   if (!input) return;
 
-  display.innerHTML = "Loading...";
+    display.innerHTML = "Let me look in the back...";
 
   try {
     const res = await fetch(`https://pokeapi.co/api/v2/item/${input}`);
@@ -30,7 +30,7 @@ async function fetchItem(itemName = null) {
 
     const data = await res.json();
     const name = data.name;
-    const img = data.sprites?.default || ''; //First use of optional chaining so that it doesn't break when it can't find anything
+    const img = data.sprites?.default; //First use of optional chaining so that it doesn't break if it can't find anything
     const effect = data.effect_entries.find(e => e.language.name === 'en')?.short_effect || 'No effect info.';
     const cost = data.cost;
 
@@ -40,36 +40,13 @@ async function fetchItem(itemName = null) {
       ${img ? `<img src="${img}" alt="${name}" class="item-img" />` : ''} 
       <p class="effect">${effect}</p>
       <p class="cost">cost= $${cost}</>
+      
     `;
   } catch (error) {
-    display.innerHTML = `<p style="color: red;">${error.message}</p>`;
+    display.innerHTML = `<p style="color: red;">${error.message}</p>`; 
     console.error("Error fetching item:", error);
   }
 }
 
-// Load the item list when the page loads
 window.onload = fetchItemList;
 
-
-
-// //css test stuff
-//  #map {
-//       padding-left: 20px;
-//       margin-top: 1200px;
-//       margin-right: 100px
-//       width: 100px
-//     }
-//     #celadon {
-//       margin-right: 200px;
-//       margin-top: 1200px;
-//       width: 100px
-//     }
-
-// </div>
-//    <div id="map">
-//     <img src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-blue-version/5/52/Pokem_170.jpg?width=325&format=jpg&auto=webp&quality=80">
-//   </div>
-
-//   <div id="celadon">
-//     <img src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-blue-version/0/01/Pokem_173.jpg?width=325&format=jpg&auto=webp&quality=80">
-//   </div>
